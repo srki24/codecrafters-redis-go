ea0a88cf9835285c65eb8b0c697976bd081f156c
srki24 <25962029+srki24@users.noreply.github.com>
2026-09-07 21:42:54 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -17,9 +17,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	conn.Write([]byte("PONG\r\n"))
+
 }
